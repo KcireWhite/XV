051a4e6a54a579f4e8b52e1b1d4cbcd409a750dc
KcireWhite <72841030+KcireWhite@users.noreply.github.com>
2024-11-15 20:12:19 -0600

    Add files via upload

---
+++ JS/Emergente.js
@@ -0,0 +1,23 @@
+   // Función que muestra la ventana emergente
+    window.onload = function() {
+        const modal = document.getElementById('modal');
+        modal.style.display = 'block'; // Muestra la ventana emergente al cargar
+    };
+
+    // Función para ocultar la ventana emergente y reproducir el audio
+    function startPage() {
+        const modal = document.getElementById('modal');
+        const audio = document.getElementById('reproductor');
+        
+        
+        // Ocultar la ventana emergente
+        modal.style.display = 'none'; 
+        
+        // Reproducir el audio
+        audio.play().then(() => {
+            document.getElementById('reproductor').volume = 0.1;
+            console.log('Audio iniciado.');
+        }).catch(error => {
+            console.log('Error al reproducir el audio:', error);
+        });
+    }
