f09c74f93f37502b91dfacd1dc5a8233d28abdf5
KcireWhite <72841030+KcireWhite@users.noreply.github.com>
2024-11-15 21:10:47 -0600

    Update Emergente.js
    Retraso JS

--- JS/Emergente.js
+++ JS/Emergente.js
@@ -1,23 +1,25 @@
-   // Función que muestra la ventana emergente
-    window.onload = function() {
-        const modal = document.getElementById('modal');
-        modal.style.display = 'block'; // Muestra la ventana emergente al cargar
-    };
+// Función que muestra la ventana emergente
+window.addEventListener('load', () => {
+    // Aseguramos que todo el contenido haya cargado antes de mostrarlo
+    document.body.classList.add('loaded'); // Agrega la clase que hace visible el contenido
+    const modal = document.getElementById('modal');
+    modal.style.display = 'block'; // Muestra la ventana emergente al cargar
+});
 
-    // Función para ocultar la ventana emergente y reproducir el audio
-    function startPage() {
-        const modal = document.getElementById('modal');
-        const audio = document.getElementById('reproductor');
-        
-        
-        // Ocultar la ventana emergente
-        modal.style.display = 'none'; 
-        
+// Función para ocultar la ventana emergente y reproducir el audio
+async function startPage() {
+    const modal = document.getElementById('modal');
+    const audio = document.getElementById('reproductor');
+
+    // Ocultar la ventana emergente
+    modal.style.display = 'none'; 
+
+    try {
         // Reproducir el audio
-        audio.play().then(() => {
-            document.getElementById('reproductor').volume = 0.6;
-            console.log('Audio iniciado.');
-        }).catch(error => {
-            console.log('Error al reproducir el audio:', error);
-        });
+        await audio.play();
+        audio.volume = 0.1;
+        console.log('Audio iniciado.');
+    } catch (error) {
+        console.log('Error al reproducir el audio:', error);
     }
+}
